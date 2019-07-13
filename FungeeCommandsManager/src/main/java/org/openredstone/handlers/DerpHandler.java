4516f332447c2ffaf25f1eda07cd430d22f6bfb6
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2019-07-13 13:27:50 +0300

    Get rid of FileArrayProvider

--- FungeeCommandsManager/src/main/java/org/openredstone/handlers/DerpHandler.java
+++ FungeeCommandsManager/src/main/java/org/openredstone/handlers/DerpHandler.java
@@ -4,29 +4,29 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.openredstone.FungeeCommandsManager;
-import org.openredstone.utils.FileArrayProvider;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 
 public class DerpHandler  {
 
-    private static final File derpsFile =  new File(FungeeCommandsManager.pluginFolder + File.separator + "Derps.txt");
-    private static String[] derps;
+    private static final File derpsFile =  new File(FungeeCommandsManager.pluginFolder, "Derps.txt");
+    private static List<String> derps;
     private static Random rand = new Random();
 
     public static void loadDerps(){
-
-        if(!(derpsFile.exists())) {
+        if (!derpsFile.exists()) {
             FungeeCommandsManager.logger.log(Level.WARNING, "No Derps.txt Detected! This is an ERROR on your part!");
-        }else{
-            try {
-                derps = FileArrayProvider.readLines(derpsFile);
-            } catch (IOException e) {
-                FungeeCommandsManager.logger.log(Level.WARNING, "Error reading Derps.txt", e);
-            }
+            return;
+        }
+        try {
+            derps = Files.readAllLines(derpsFile.toPath());
+        } catch (IOException e) {
+            FungeeCommandsManager.logger.log(Level.WARNING, "Error reading Derps.txt", e);
         }
 
     }
@@ -34,16 +34,18 @@ public class DerpHandler  {
     public static void sendDerp(CommandSender sender, String[] args){
         String derpPrefix = ChatColor.DARK_GREEN + " * " + ChatColor.WHITE + sender.getName() + ChatColor.DARK_BLUE + " DERP!  " + ChatColor.LIGHT_PURPLE;
         try {
-            sender.sendMessage(new TextComponent(derpPrefix + derps[Integer.parseInt(args[0])]));
+            sender.sendMessage(new TextComponent(derpPrefix + derps.get(Integer.parseInt(args[0]))));
         } catch(Exception e) {
-            sender.sendMessage(new TextComponent(derpPrefix + derps[rand.nextInt(derps.length-1) + 1]));
+            sender.sendMessage(new TextComponent(derpPrefix + derps.get(rand.nextInt(derps.size() - 1) + 1)));
         }
     }
 
     public static void getDerpList(CommandSender sender){
-        for(int temp = 0; temp<=derps.length-1;temp++){
-            sender.sendMessage(new TextComponent(temp + ". " + derps[temp]));
+        // TODO: figure out a java way of doing this
+        int index = 0;
+        for (String derp : derps) {
+            sender.sendMessage(new TextComponent(index + ". " + derp));
+            ++index;
         }
     }
-
 }
